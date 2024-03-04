99df34c7af12c50efb24b60b9d101ef2cfc78399
fullmoonemptysun <v685958@gmail.com>
2024-03-04 11:59:44 -0700

    add clear button, create basic mathematical functions

---
+++ calc.js
@@ -0,0 +1,29 @@
+function renderApp(){
+    //Selecting nodes
+    const strExp = document.querySelector(".displayStr");
+    const strResult = document.querySelector(".resultStr");
+
+
+}
+
+function operate(op, num1, num2){
+    
+}
+
+
+function initButtons(){
+    //number buttons 
+}
+
+function add(num1, num2){
+    return num1 + num2;
+}
+function sub(num1, num2){
+    return num1 - num2;
+}
+function mult(num1, num2){
+    return num1 * num2;
+}
+function div(num1, num2){
+    return num1/num2;
+}
