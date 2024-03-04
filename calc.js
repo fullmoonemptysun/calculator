98b743de7e81112f5eed6f7cf2b9c1a588ae10a7
fullmoonemptysun <v685958@gmail.com>
2024-03-04 14:45:21 -0700

    add bg add expression update functionality using runningValues array

--- calc.js
+++ calc.js
@@ -1,19 +1,30 @@
-function renderApp(){
-    //Selecting nodes
-    const strExp = document.querySelector(".displayStr");
-    const strResult = document.querySelector(".resultStr");
+let runningValues = [0];
+const strExp = document.querySelector(".exp-str");
+updateExp();
 
 
+let numButtons = document.querySelectorAll(".num");
+numButtons = Array.from(numButtons);
+numButtons.forEach((numBtn) => numBtn.addEventListener('click', function(e){
+
+        
+        if(runningValues[0] == 0){
+            runningValues[0] = (Number)(numBtn.getAttribute("value"));
+        }
+
+        else{
+            runningValues.push((Number)(numBtn.getAttribute("value")));
+        }
+
+    updateExp();
+}))
+
+
+
+function updateExp(){
+    strExp.textContent = (Number)(runningValues.join(""));
 }
 
-function operate(op, num1, num2){
-    
-}
-
-
-function initButtons(){
-    //number buttons 
-}
 
 function add(num1, num2){
     return num1 + num2;
@@ -27,3 +38,17 @@ function mult(num1, num2){
 function div(num1, num2){
     return num1/num2;
 }
+
+function eq(){
+
+}
+
+function reset(){
+
+}
+
+
+
+
+
+
